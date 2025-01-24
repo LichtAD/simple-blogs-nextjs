83941924990eb84345409f855aa763e30bd13537
LichtAD <antu.dutta@g.bracu.ac.bd>
2025-01-24 19:55:50 +0600

    first commit

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function Products() {
+    return (
+        <div>
+            <h1>Products</h1>
+            <h4>products 1</h4>
+            <h4>products 2</h4>
+            <h4>products 3</h4>
+        </div>
+    );
+}
